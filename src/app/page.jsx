e48ad0425fd686218f1ae5faa6a60edbda808f03
eric-dias <124622413+eric-dias@users.noreply.github.com>
2024-02-02 11:52:11 -0300

    start principlesCard

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,59 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
-import HomeTopContainer from "@/components/homeTopContainer/homeTopContainer";
 
 import projectsJson from "../../public/projects.json";
+import principlesJson from "../../public/principles.json";
+
 import ProjectCard from "@/components/projectCard/projectCard";
+
+import { FaArrowRight } from "react-icons/fa6";
+import PrinciplesCard from "@/components/principlesCard/principlesCard";
 
 export default function Home() {
   //console.log(projects);
 
   const firstProjects = projectsJson.projects.slice(0, 4);
+  const principles = principlesJson.principles
 
   return (
     <main className={styles.main}>
-      <HomeTopContainer />
+      <div className={styles.topContainer}>
+        <h1 className={styles.title}>
+          We create better spaces for you to live&nbsp;
+          <span className={styles.yellowSpan}>+</span> play.
+        </h1>
+        <h2 className={styles.subtitle}>
+          For over a decade, we've provided Fraser Valley homeowners like you
+          with high-end, custom renovations and additions.
+        </h2>
+        <Link
+          href="/work"
+          key="work"
+          className={`${styles.link} ${styles.contactLink}`}
+        >
+          See our work &nbsp;
+          <FaArrowRight className={styles.icon} />
+        </Link>
+      </div>
+
       <div className={styles.projectsContainer}>
         {firstProjects.map((project) => (
           <ProjectCard project={project} key={project.id} />
+        ))}
+      </div>
+
+      <div className={styles.midContainer}>
+        <h1 className={styles.title}>Vision. Clarity. Results.</h1>
+        <h2 className={styles.subtitle}>
+          Our client relationships are built on trust, collaboration, and
+          transparency. With your ideas and our guidance and resources, the
+          possibilities are endless.
+        </h2>
+      </div>
+
+      <div className={styles.principlesContainer}>
+        {principles.map((principle) => (
+          <PrinciplesCard key={principle.id} principle={principle}/>
         ))}
       </div>
     </main>
---
+++ src/components/principlesCard/principlesCard.jsx
@@ -0,0 +1,22 @@
+import styles from "./PrinciplesCard.module.css";
+
+const PrinciplesCard = ({ principle }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <img
+          className={styles.image}
+          src={principle.img}
+          alt={principle.title}
+        />
+      </div>
+
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>{principle.title}</h1>
+        <p className={styles.description}>{principle.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default PrinciplesCard;
